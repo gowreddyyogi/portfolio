de75536abf2f75ab50ea04469217410832dfd2fc
gowreddyyogi <gowreddyyogi@gmail.com>
2026-09-01 09:52:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// ================= MOBILE MENU =================
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+
+    const icon = menuBtn.querySelector("i");
+
+    if (navLinks.classList.contains("active")) {
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+    } else {
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    }
+});
+
+
+// Close menu after clicking a link
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        const icon = menuBtn.querySelector("i");
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    });
+
+});
+
+
+// ================= CURRENT YEAR =================
+
+document.getElementById("year").textContent =
+    new Date().getFullYear();
